275cedd1a89c1c9aaa3e1443711ff6198fb76a86
billsegall <bill@segall.net>
2015-06-10 14:42:16 +1000

    For release builds collect packages into 'release'.
    This stops packages from being overwritten so we can gather the release
    build outputs for downloading from cobaro.org.

--- script/build.sh
+++ script/build.sh
@@ -22,5 +22,38 @@ make distcheck
 make package
 (cd test; make valgrind)
 
+# Stash the build output of release builds so we can easily fetch them
+# and they don't get overwritten by other builds
+case "$GIT_BRANCH" in
+origin/release/*)
+    RELEASE=${GIT_BRANCH#origin/release}
+    TODAY=`date +%Y%m%d`
+    SYSTYPE=`/usr/local/bin/systype.py -d`
+
+    case "$PLATFORM" in
+    Ubuntu|Debian)
+        PACKAGES="libcobaro-trace0*_${RELEASE}_amd64.deb"
+        ;;
+    Centos/Redhat)
+        PACKAGES="packages/rpm/RPMS/x86_64/libcobaro-trace0*-${RELEASE}-${TODAY}.x86_64.rpm"
+        ;;
+    Centos/Redhat)
+        PACKAGES="packages/darwin/libcobaro-trace0-*.pkg"
+        ;;
+    *)
+        echo "Unrecognised platform: $SYSTYPE"
+        exit 1
+        ;;
+    esac
+
+    if [ -n "$PACKAGES" ]; then
+        mkdir -p releases
+        cp $PACKAGES releases
+    fi
+
+*)
+    ;;
+esac
+
 echo "Build passed"
 exit 0
