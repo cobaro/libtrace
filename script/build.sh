bb9e340ddb9f920c2db56df4a4b1ce6f255cd2a6
billsegall <bill@segall.net>
2015-06-10 13:56:15 +1000

    Make scripting easier by using set -e and a trapfunc

--- script/build.sh
+++ script/build.sh
@@ -4,12 +4,23 @@
 env
 set -x
 
-export CFLAGS='-Wall -Werror -O2 -g -pipe -std=gnu99'
-./bootstrap && \
-./configure && \
-make && \
-make distcheck && \
-make package && \
-(cd test; make valgrind) && \
-echo "Build passed" || \
-(echo "Build failed" && exit 1)
+# For convenience we set us up to fail on any error.
+# and use a trapfunc to let us know abotu failure
+function trapfunc {
+    set +e
+    echo Build failed
+    exit 1
+}
+trap trapfunc ERR
+set -e
+
+export CFLAGS='-Wall -Werror -O2 -g -pipe -std=c99'
+./bootstrap
+./configure
+make
+make distcheck
+make package
+(cd test; make valgrind)
+
+echo "Build passed"
+exit 0
